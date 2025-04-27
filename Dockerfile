827f08df5123591747cd87a2a5f43be5a2e775c3
fahim-arif <fahim1.618555@gmail.com>
2025-04-28 04:22:14 +0600

    main: scaffolding

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# syntax=docker/dockerfile:1        <-- must start with “#” and be line-1
+
+FROM golang:1.24-alpine AS builder
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=0 go build -o server .
+
+FROM alpine:3.20
+WORKDIR /app
+COPY --from=builder /app/server .
+EXPOSE 3000
+CMD ["./server"]
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"main/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:3000
+}
